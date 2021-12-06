4fdca33ff49e54a0144551b866ae0c7b0ab664dc
Uğur Keskin <67828030+ugurrk35@users.noreply.github.com>
2021-12-06 17:21:26 +0300

    d

---
+++ javascript-odev-2/todo.js
@@ -0,0 +1,61 @@
+//listenin kapatma butonu
+var myList = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myList.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myList[i].appendChild(span);
+}
+
+// liste saklama
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+
+// listeye tıklanıldında sembolü ekleme
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+// yeni eleman eklema
+function newElement() {
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("task").value;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue === '') {
+      alert("Boş geçilmez birşeyler yaz!");
+    } else {
+      document.getElementById("list").appendChild(li);
+   
+      localStorage.setItem('list',item);
+    }
+    document.getElementById("task").value = "";
+  
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+  
+    for (i = 0; i < close.length; i++) {
+      close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+      }
+    }
+  }
+
+ let item =localStorage.getItem('list');
+ 
