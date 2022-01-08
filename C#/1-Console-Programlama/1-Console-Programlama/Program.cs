fb473efbf7fd40597124c9cee267e15092a48aae
Uğur Keskin <67828030+ugurrk35@users.noreply.github.com>
2022-01-08 23:20:04 +0300

    1-Console Programlama

---
+++ C#/1-Console-Programlama/1-Console-Programlama/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello World!");
+Console.WriteLine("İsminizi giriniz:");
+string name = Console.ReadLine();
+Console.WriteLine("Soyadınızı giriniz");
+string surname = Console.ReadLine();
+
+Console.WriteLine("Merhaba " + name + " " + surname);
