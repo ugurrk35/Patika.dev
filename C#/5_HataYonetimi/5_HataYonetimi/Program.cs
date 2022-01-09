1521ad730d74da36d7fc7ed0d612582b60eb3057
Uğur Keskin <67828030+ugurrk35@users.noreply.github.com>
2022-01-09 14:18:31 +0300

    Hata Yonetimi

---
+++ C#/5_HataYonetimi/5_HataYonetimi/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace _5_HataYonetimi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                try
+                {
+                    Console.WriteLine("Bir sayi giriniz:");
+                    int sayi = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Girdiginiz sayi :" + sayi);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Hata :" + ex.Message.ToString());
+                }
+                finally
+                {
+                    Console.WriteLine("Islem tamamlandi");
+                }
+
+                try
+                {
+                    //int a = int.Parse(null);
+                    //int a = int.Parse("test");
+                    int a = int.Parse("-20000000000");
+                }
+                catch (ArgumentNullException ex)
+                {
+                    Console.WriteLine("Bos bir deger girdiniz.");
+                    Console.WriteLine(ex);
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine("Veri tipi uygun degil.");
+                    Console.WriteLine(ex);
+                }
+                catch (OverflowException ex)
+                {
+                    Console.WriteLine("Cok kucuk ya da cok buyuk bir deger girdiniz.");
+                    Console.WriteLine(ex);
+                }
+                finally
+                {
+                    Console.WriteLine("Islem basariyla tamamlandi.");
+                }
+            }
+        }
+    }
+
+    
+
+
