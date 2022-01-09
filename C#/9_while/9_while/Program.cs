7f76c120e9e3c9b669b8d39878a05fb8859bd48a
Uğur Keskin <67828030+ugurrk35@users.noreply.github.com>
2022-01-09 17:24:33 +0300

    while

---
+++ C#/9_while/9_while/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _9_while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen bir sayı giriniz:");
+            int sayi = int.Parse(Console.ReadLine());
+            int sayac = 1;
+            int toplam = 0;
+
+            while (sayac <= sayi)
+            {
+                toplam += sayac;
+                sayac++;
+            }
+
+            Console.WriteLine(toplam / sayi);
+
+            // 'a' dan 'z' ye kadar tüm harfleri console'a yazdır.
+
+            char ch = 'a';
+            while (ch <= 'z')
+            {
+                Console.WriteLine(ch);
+                ch++;
+            }
+
+            Console.WriteLine("-----Foreach-----");
+            string[] arabalar = { "BMV", "Ford", "Toyota", "Nissan" };
+
+            foreach (var araba in arabalar)
+            {
+                Console.WriteLine(araba);
+            }
+        }
+    }
+}
+
