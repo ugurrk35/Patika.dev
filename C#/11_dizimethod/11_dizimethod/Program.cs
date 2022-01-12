53b254274274b3283d8c9b3536d8447f0c0830b0
Uğur Keskin <67828030+ugurrk35@users.noreply.github.com>
2022-01-12 12:58:35 +0300

    dizilerde method

---
+++ C#/11_dizimethod/11_dizimethod/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace _11_dizimethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //sort arrays
+            int[] numbers = { 2, 21, 45, 68, 88, 4, 13, 22 };
+
+            Console.WriteLine("Unsorted Array");
+
+            foreach (var item in numbers)
+                Console.WriteLine(item);
+
+            Console.WriteLine("Sorted Array");
+            Array.Sort(numbers);
+
+            foreach (var item in numbers)
+                Console.WriteLine(item);
+
+
+            //clear arrays
+
+            Console.WriteLine("Clear Array");
+            //clear 2 elements starts from 2nd indice 
+            Array.Clear(numbers, 2, 2);
+
+            foreach (var item in numbers)
+                Console.WriteLine(item);
+
+            //reverse arrays
+
+            Console.WriteLine("Reverse Array");
+            Array.Reverse(numbers);
+
+            foreach (var item in numbers)
+                Console.WriteLine(item);
+
+            //indexof array
+            Console.WriteLine("Array IndexOf");
+            Console.WriteLine("Index Of 23: ", Array.IndexOf(numbers, 23));
+
+            //resize array
+
+            Console.WriteLine("Resize Array");
+            Array.Resize<int>(ref numbers, 9);
+            numbers[8] = 99;
+
+            Console.WriteLine("New array will be: ");
+            foreach (var item in numbers)
+                Console.WriteLine(item);
+        }
+    }
+}
+
